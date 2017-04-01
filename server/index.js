66e4454aac525230319bd3312b9bdeb068f0b534
xavier-rdo <xavier.roldo@elao.com>
2017-04-01 16:37:22 +0200

    Webpack-dev-server replaced by Node Express

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
+
+let app = express();
+
+app.use(bodyParser.json());
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
+
+app.use('/build', express.static(__dirname + '/public/build'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(3000, () => console.log('Running on localhost:3000'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtools: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/server/public/build/js/'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'client')
+        ],
+        loaders: [ 'react-hot', 'babel' ]
+      }
+    ]
+  },
+  resolve: {
+    extentions: [ '', '.js' ]
+  }
+}
