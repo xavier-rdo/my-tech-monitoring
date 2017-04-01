273c347e451afb97535398ec7d367f55356db834
xavier-rdo <xavier.roldo@elao.com>
2017-04-01 19:11:53 +0200

    Navigation bar moved in a dedicated React component

--- client/App.js
+++ client/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavigationBar from './NavigationBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavigationBar />
       </div>
     );
   }
