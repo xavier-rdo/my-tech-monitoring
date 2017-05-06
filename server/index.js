92bcafd3da9e578d6cdc89ea7da457fc99f10d08
xavier-rdo <xavier.roldo@elao.com>
2017-05-06 16:53:09 +0200

    [Server] Express routes bootstrapped, index.js cleaned up

--- server/index.js
+++ server/index.js
@@ -8,15 +8,22 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
-import TechResourcesRepository from './model/TechResourcesRepository';
-
-const techResourcesRepository = new TechResourcesRepository();
+import techResources from './routes/techresources';
+import users from './routes/users';
 
 let app = express();
 
-app.disable('x-powered-by'); // cf. http://expressjs.com/en/advanced/best-practice-security.html
+// @see http://expressjs.com/en/advanced/best-practice-security.html
+app.disable('x-powered-by');
+
 app.use(bodyParser.json());
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
+
+/* ***************************** */
+/* Webpack dev & hot middlewares */
+/* ***************************** */
+
+// @see https://github.com/glenjamin/webpack-hot-middleware
 
 const compiler = webpack(webpackConfig);
 
@@ -31,26 +38,16 @@ app.use(webpackHotMiddleware(compiler));
 /* Serve static content from public build directory : */
 app.use('/build', express.static(__dirname + '/public/build'));
 
-// @todo: move this in a dedicated module cf. http://expressjs.com/en/guide/routing.html#express-router */
-app.get('/api/techresources', (req, res) => {
-    techResourcesRepository.getAllResources()
-        .then(function(data) {
-            res.status(200)
-               .json({
-                    status: 'success',
-                    data: data,
-                    message: 'Found'
-                });
-        })
-        .catch(function (err) {
-            res.status(500)
-               .json({
-                    status: 'failure',
-                    message: 'Database request has failed'
-                });
-        });
-    ;
-});
+/* *************************************** */
+/* Routes for API (tech resources & users) */
+/* *************************************** */
+
+app.use('/api/techresources', techResources);
+app.use('/api/users', users);
+
+/* *************************************** */
+/* Render index.html (React.js driven SPA) */
+/* *************************************** */
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
---
+++ server/routes/techresources.js
@@ -0,0 +1,29 @@
+import express from 'express';
+const router = express.Router();
+
+import TechResourcesRepository from '../model/TechResourcesRepository';
+const techResourcesRepository = new TechResourcesRepository();
+
+/**
+ * Get all technical resources
+ */
+router.get('/', (req, res) => {
+    techResourcesRepository.getAllResources()
+        .then(function(data) {
+            res.status(200)
+               .json({
+                    data: data,
+                    pagination: {}
+                });
+        })
+        .catch(function (err) {
+            res.status(500)
+               .json({
+                    errorCode: 1020,
+                    errorMessage: 'Database request has failed : ' + err
+                });
+        });
+    ;
+});
+
+module.exports = router;
