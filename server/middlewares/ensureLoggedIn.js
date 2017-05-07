ad3dc441097cd042166da0a3d25980e8067e1297
xavier-rdo <xavier.roldo@elao.com>
2017-05-07 20:54:58 +0200

    [Server] Some user routes bootstrapped & ensureLoggedIn middleware (partially) implemented

---
+++ server/middlewares/ensureLoggedIn.js
@@ -0,0 +1,50 @@
+/**
+ * Middleware that makes sure that current user is authenticated:
+ *
+ * - attach current user to request if she/he is authenticated
+ * - return a 401 HTTP response otherwise.
+ *
+ * Middleware inspired from Reddice. @see https://github.com/Remchi/reddice/blob/master/server/middlewares/authenticate.js
+ *
+ * @type {Express~req}
+ * @type {Express~res}
+ *
+ * @param  {Express~req} req
+ * @param  {Express~res} res
+ * @param  {Function}    next
+ */
+export default (req, res, next) => {
+    const authorizationHeader = req.headers['authorization'];
+    let token;
+
+    if (authorizationHeader) {
+      token = authorizationHeader.split(' ')[1];
+    }
+
+    if (token) {
+        // @todo: retrieve current user from JWT
+        if (false === /please/i.test(token)) {
+            res.status(404).json({
+                'errorCode': 404,
+                'errorMessage': 'User not found from token'
+            });
+        }
+
+        const user = {
+            id: 1,
+            username: 'John Doe',
+            email: 'john.doe@gmail.com',
+            isAdmin: false,
+            avatar: null
+        };
+        // @todo: retrieve his counter-part in DB storage and return 404 if not found
+        req.currentUser = user;
+        next();
+    } else {
+        // @todo: isolate error codes and messages in a dedicated JS module
+        res.status(401).json({
+            'errorCode': 401,
+            'errorMessage': 'Unauthorized. Authentication required.'
+        });
+    }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,44 @@
 import express from 'express';
+import ensureLoggedIn from '../middlewares/ensureLoggedIn';
+
 const router = express.Router();
+
+/**
+ * Handle user signup (registration). Also grant JWT to signed-up user.
+ */
+router.post('/signup', (req, res) => {
+    // @todo: unimplemented yet
+    res.status(201).json({
+        'message': 'Created'
+    });
+});
+
+/**
+ * Handle user signin (login). Also grant JWT to signed-in user.
+ */
+router.post('/signin', (req, res) => {
+    // @todo: unimplemented yet
+    res.status(204).end();
+});
+
+/**
+ * Handle current user's profile update.
+ */
+router.patch('/', ensureLoggedIn, (req, res) => {
+    // @todo: unimplemented yet
+    const currentUser = req.currentUser;
+
+    res.status(204).end();
+});
+
+/**
+ * Handle current user's password reset
+ */
+router.patch('/reset-password', ensureLoggedIn, (req, res) => {
+    // @todo: unimplemented yet
+    const currentUser = req.currentUser;
+
+    return res.status(204).end();
+});
 
 module.exports = router;
