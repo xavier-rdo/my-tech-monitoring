a7a585c0d917b1eace9be04cd0064a1b730497fd
xavier-rdo <xavier.roldo@elao.com>
2017-05-09 20:11:23 +0200

    [ESLint] webpack config file lint-fixed

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,62 +2,62 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtools: 'eval-source-map',
-  watch: true,
-  entry: [
-    'babel-polyfill',
-    'react-hot-loader/patch',
-    'webpack-hot-middleware/client',
-    path.join(__dirname, '/client/index.js')
-  ],
-  output: {
-    path: path.join(__dirname, '/server/public/build/js/'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        include: [
-          path.join(__dirname, 'client')
-        ],
-        loaders: [ 'babel' ]
-      },
-      {
-        test: /\.css$/,
-        loaders: [ 'style', 'css', 'sass' ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
+    devtools: 'eval-source-map',
+    watch: true,
+    entry: [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/server/public/build/js/'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
         loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader'
+            {
+                test: /\.js$/,
+                include: [
+                    path.join(__dirname, 'client')
+                ],
+                loaders: [ 'babel' ]
+            },
+            {
+                test: /\.css$/,
+                loaders: [ 'style', 'css', 'sass' ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack-loader'
+                ]
+            }
         ]
-      }
-    ]
-  },
-  imageWebpackLoader: {
-    gifsicle: {
-      interlaced: false
     },
-    optipng: {
-      optimizationLevel: 7
+    imageWebpackLoader: {
+        gifsicle: {
+            interlaced: false
+        },
+        optipng: {
+            optimizationLevel: 7
+        },
+        mozjpeg: {
+            quality: 65
+        },
+        pngquant:{
+            quality: '65-90',
+            speed: 4
+        }
     },
-    mozjpeg: {
-      quality: 65
-    },
-    pngquant:{
-      quality: "65-90",
-      speed: 4
+    resolve: {
+        extentions: [ '', '.js' ]
     }
-  },
-  resolve: {
-    extentions: [ '', '.js' ]
-  }
-}
+};
